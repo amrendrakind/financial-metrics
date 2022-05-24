e9ae467fd7eaee1ccb31b1e6d0466cb1dc160251
amrendrakind <vision2010@gmail.com>
2022-05-24 07:12:57 +0530

    API Data Fetching info

---
+++ src/redux/APIServices.js
@@ -0,0 +1,18 @@
+const companySymbolAPI = 'https://financialmodelingprep.com/api/v3/nasdaq_constituent?apikey=b910dda5a91116817f2abf37854a3177';
+
+// https://financialmodelingprep.com/api/v3/dowjones_constituent?apikey=YOUR_API_KEY
+
+// https://financialmodelingprep.com/api/v3/sp500_constituent?apikey=YOUR_API_KEY
+
+const getcompanySymbolData = async () => {
+  const response = await fetch(companySymbolAPI, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  const companySymbolData = await response.json();
+  return companySymbolData;
+};
+
+export default { getcompanySymbolData };
