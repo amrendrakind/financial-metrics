c664ed4a110390e9aeb643345af5ec99ed1b2974
amrendrakind <vision2010@gmail.com>
2022-05-26 22:21:10 +0530

    Mobile responsiveness and styling

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './NavBar.css';
+import { FaHome } from 'react-icons/fa';
 import image from '../../logo-finance.png';
 
 const Navbar = () => {
@@ -18,23 +19,35 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="nav-bar">
-
-      <div className="brand-logo-container">
-        <img className="spacelogo" src={image} alt="spacelogo" />
-        <h2 className="nav-brand">Financial Metrices</h2>
-      </div>
-      <ul className="nav-links">
-        {links.map((link) => (
-          <li className="nav-link" key={link.id}>
-            <NavLink to={link.path}>
-              {link.text}
+    <>
+      <nav className="nav-bar">
+        <div className="brand-logo-container">
+          <img className="spacelogo" src={image} alt="spacelogo" />
+          <h2 className="nav-brand">Financial Metrices</h2>
+        </div>
+        <ul className="nav-links">
+          {links.map((link) => (
+            <li className="nav-link" key={link.id}>
+              <NavLink to={link.path}>
+                {link.text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <nav className="mobile-section">
+        <div className="mobile-header">
+          <div className="mobile-homeIcon">
+            <NavLink to="/financial-metrics">
+              <FaHome className="mobile-home" />
             </NavLink>
-          </li>
-        ))}
-
-      </ul>
-    </nav>
+          </div>
+          <div className="titleHeader">
+            Financial Metrices
+          </div>
+        </div>
+      </nav>
+    </>
   );
 };
 export default Navbar;
